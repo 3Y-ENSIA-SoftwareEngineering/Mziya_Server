ff2a5885a9cdb80daa3cf83f476feb2cefb4705d
YasmineNaimaSenour <yasmine.naima.senour@ensia.edu.dz>
2024-12-11 07:27:48 +0100

    use emport export instead of require

--- BackEnd/src/routes/jobRoutes.js
+++ BackEnd/src/routes/jobRoutes.js
@@ -1,15 +1,18 @@
-const express = require('express');
-const JobController = require('../controllers/jobController');
-const validateJobInput = require('../middleware/validatJobInput');
-
+import express from 'express';
+import JobController from '../controllers/jobController.js';
+import validateJobInput from '../middleware/validatJobInput.js';
 
 const router = express.Router();
 
 // Route to create a new job
-router.post('/addjob', validateJobInput, (req, res, next) => {
-    console.log(`[${new Date().toISOString()}] Received POST request to /addjob with body:`, req.body);
-    next();
-}, JobController.addJob);
+    router.post(
+    '/addjob', 
+    validateJobInput, 
+    (req, res, next) => {
+        console.log(`[${new Date().toISOString()}] Received POST request to /addjob with body:`, req.body);
+        next();
+    }, 
+    JobController.addJob
+    );
 
-
-module.exports = router;
+export default router;
